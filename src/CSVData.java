19d15ff51da510259af2ee3aa2d4dd50c3dfd451
amarquezvera872 <amarquezvera872@fhs.win.fuhsd.org>
2016-10-12 10:41:51 -0700

    method stubs added

--- src/CSVData.java
+++ src/CSVData.java
@@ -1,4 +1,18 @@
 
 public class CSVData {
-
+	private double[][] data;
+	private String[] columnNames;
+	/***
+	 * takes a file and turns it into a CSVData object
+	 * @param filename the file to read
+	 * @param numLinesToIgnore How many lines at top to ignore
+	 * @param columnNames names of each column
+	 * @return a CSVData object for that file
+	 */
+	public static CSVData readCSVFile(String filename, int numLinesToIgnore, String[] columnNames){
+	CSVData result = null;
+	
+	return result;
+	}
+	
 }
